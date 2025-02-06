8ab046c4826edd9078035499039091a284edb908
Jhordy Andres Agaton Penagos <JAGATON@BDO.COM.CO>
2025-02-06 17:33:25 -0500

    Add endpoint role

--- app/main.py
+++ app/main.py
@@ -3,8 +3,15 @@ from fastapi.responses import JSONResponse
 from fastapi_jwt_auth.exceptions import AuthJWTException
 from contextlib import asynccontextmanager
 
+from app.db.Database_Connection_ORM import DatabaseConnectionORM
 from app.security import auth
-from app.api.v1.endpoints import user
+from app.api.v1.endpoints import role, user
+
+db = DatabaseConnectionORM()
+Base = db.get_base()
+engine = db.get_engine()
+Base.metadata.create_all(engine)
+session = db.get_session()
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -26,6 +33,7 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 app.include_router(auth.router, prefix="/auth")
 app.include_router(user.router, prefix="/user")
+app.include_router(role.router, prefix="/role")
 
 @app.exception_handler(AuthJWTException)
 def authjwt_exception_handler(request: Request, exc: AuthJWTException):
