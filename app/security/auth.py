8e6c71f5d6e00f49397b9a9addd585c38f9972dd
Jhordy Andres Agaton Penagos <JAGATON@BDO.COM.CO>
2025-02-06 16:22:48 -0500

    Replace strings by DTO

--- app/security/auth.py
+++ app/security/auth.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from fastapi import HTTPException
 from fastapi import Depends
+from app.schemas.user import UserCreate
 from app.services.Auth_Service import AuthService
 from app.services.User_Service import UserService
 from app.db.Database_Connection_ORM import DatabaseConnectionORM
@@ -13,24 +14,24 @@ def get_session(session=Depends(DatabaseConnectionORM().get_session)) -> AuthSer
     return session
 
 @router.post("/login")
-def login(username: str, password: str, session: Session = Depends(get_session)):
+def login(user: UserCreate, session: Session = Depends(get_session)):
     auth_service = AuthService(session)
-    if auth_service.validate_user(username,password):
-        access_token = auth_service.create_access_token(data={"sub": username})
+    if auth_service.validate_user(user.username,user.password):
+        access_token = auth_service.create_access_token(data={"sub": user.username})
         return {"access_token": access_token, "token_type": "bearer"}
     else:
-        raise HTTPException(status_code=401, detail="Credenciales incorrectas")
+        raise HTTPException(status_code=401, detail="Unauthorized")
     
 @router.post("/register")
-def register(username: str, password: str, session: Session = Depends(get_session), auth: AuthJWT = Depends()):
+def register(user: UserCreate, session: Session = Depends(get_session), auth: AuthJWT = Depends()):
     
     auth_service = AuthService(session)
     user_service = UserService(session)
     user_data = AuthService.validate_token(auth)
     print(user_data)
-    if not auth_service.validate_user(username,password):
-        password_encode = auth_service.get_password_hash(password)
-        user_service.create_user(username, password_encode)
-        raise HTTPException(status_code=201, detail="Usuario creado")
+    if not auth_service.validate_user(user.username,user.password):
+        password_encode = auth_service.get_password_hash(user.password)
+        user_service.create_user(user.username, password_encode)
+        raise HTTPException(status_code=201, detail="User registered")
     else:
-        raise HTTPException(status_code=400, detail="Error en la creación del usuario")
+        raise HTTPException(status_code=400, detail="Error registering user")
