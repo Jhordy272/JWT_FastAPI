4c17b9721ccbd719017f355a9cb1ca93b362c5fb
Jhordy Andres Agaton Penagos <JAGATON@BDO.COM.CO>
2025-02-06 16:25:29 -0500

    Add methods get_user_by_id, get_users, get_users_limited, update_user, delete_user

--- app/services/User_Service.py
+++ app/services/User_Service.py
@@ -1,5 +1,4 @@
 from app.db.models.User import User
-from app.db.Database_Connection_ORM import DatabaseConnectionORM
 
 class UserService:
     def __init__(self, session):
@@ -8,6 +7,16 @@ class UserService:
     def get_user_by_username(self, username):
         return self.session.query(User).filter(User.username == username).first()
     
+    def get_user_by_id(self, id):
+        return self.session.query(User).filter(User.id == id).first()
+    
+    def get_users(self):
+        return self.session.query(User).all()
+    
+    def get_users_limited(self, page: int = 1, items: int = 10):
+        offset = (page - 1) * items
+        return self.session.query(User).limit(items).offset(offset).all()
+
     def create_user(self, username, password):
         user = self.session.query(User).filter(User.username == username).first()
         if user is None:
@@ -17,3 +26,10 @@ class UserService:
             self.session.add(user)
             self.session.commit()
         return
+    
+    def update_user(self, user: User):
+        self.session.commit()
+    
+    def delete_user(self, user : User):
+        self.session.delete(user)
+        self.session.commit()
