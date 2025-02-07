ffda4f15cc68c5c60c2c15501bcbb640075be460
Jhordy Andres Agaton Penagos <JAGATON@BDO.COM.CO>
2025-02-07 09:14:59 -0500

    Change import Rol to Role

--- app/services/Role_Service.py
+++ app/services/Role_Service.py
@@ -1,35 +1,35 @@
 from app.db.models import Role
-from app.db.models.Role import Rol
+from app.db.models.Role import Role
 
 class RoleService:
     def __init__(self, session):
         self.session = session
 
     def get_role_by_id(self, id):
-        return self.session.query(Rol).filter(Rol.id == id).first()
+        return self.session.query(Role).filter(Role.id == id).first()
 
     def get_role_by_name(self, name):
-        return self.session.query(Rol).filter(Rol.name == name).first()
+        return self.session.query(Role).filter(Role.name == name).first()
     
     def get_roles(self):
-        return self.session.query(Rol).all()
+        return self.session.query(Role).all()
     
     def get_roles_limited(self, page: int = 1, items: int = 10):
         offset = (page - 1) * items
-        return self.session.query(Rol).limit(items).offset(offset).all()
+        return self.session.query(Role).limit(items).offset(offset).all()
     
     def exists_role_by_id(self, id):
-        rol = self.session.query(Rol).filter(Rol.id == id).first()
+        rol = self.session.query(Role).filter(Role.id == id).first()
         return rol is not None
     
     def exists_role_by_name(self, name):
-        rol = self.session.query(Rol).filter(Rol.name == name).first()
-        return rol is not None
+        role = self.session.query(Role).filter(Role.name == name).first()
+        return role is not None
     
     def create_role(self, name):
-        rol = Rol()
-        rol.name = name
-        self.session.add(rol)
+        role = Role()
+        role.name = name
+        self.session.add(role)
         self.session.commit()
         return
     
